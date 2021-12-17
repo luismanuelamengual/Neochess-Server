1191baaf93af889d1be4c942ae2129291abb9320
Luis Amengual <luismanuelamengual@gmail.com>
2021-12-17 11:50:43 -0300

    Cambios en estructura de proyecto

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+const socketServer = require('http').createServer();
+const io = require('socket.io')(socketServer, {});
+const socketPort = 5000;
+
+io.on('connection', client => {
+    console.log('New incoming Connection from', client.id);
+    client.on('testMsg', function(message) {
+        console.log('Message from the client:',client.id,'->',message);
+    })
+});
+socketServer.listen(8080);
